8da973ca66c537878edd8b746e5648635490d8d1
duongminhthuan2003 <thuan.duongminh2003@gmail.com>
2025-07-08 14:31:19 +0700

    Update SettingProfile.tsx
    Add logout function and a context menu for setting profile

--- src/pages/SettingsProfile.tsx
+++ src/pages/SettingsProfile.tsx
@@ -2,13 +2,63 @@ import {HugeiconsIcon} from "@hugeicons/react";
 import {ArrowRight01Icon} from "@hugeicons/core-free-icons";
 import avatar from "../assets/account/Sequoia-Sunrise.png"
 import Button from "../components/Button.tsx";
+import Footer from "../components/Footer.tsx";
+import {AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+import {useNavigate} from "react-router-dom";
 
+export function ContextMenu() {
+    const navigate = useNavigate();
+
+    return (
+        <motion.div
+            initial={{ opacity: 0, y: -30 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -30 }}
+            transition={{ duration: 0.2 }}
+            className="flex flex-col navbar mx-8 rounded-xl px-3 pt-5 pb-3 gap-5 font-BeVietnamRegular items-center">
+            <div>
+                Hồ sơ của tôi
+            </div>
+            <div>
+                [!]Đơn hàng của tôi
+            </div>
+            <div>
+                [!]Phương thức thanh toán
+            </div>
+            <div>
+                [!]Đổi mật khẩu
+            </div>
+            <div className="bg-red-500 text-white w-full flex items-center justify-center py-3 rounded-lg"
+            onClick={() => {
+                localStorage.setItem('isLoggedIn', 'false');
+                localStorage.setItem('userData', JSON.stringify({name: '', email: ''}));
+                navigate('/');
+            }}>
+                Đăng xuất
+            </div>
+        </motion.div>
+    )
+}
 function SettingsProfile() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return(
         <div>
             <div className="h-14"> </div>
-            <div className="flex flex-row mx-8 gap-2 mt-8">
-                <HugeiconsIcon icon={ArrowRight01Icon} />
+
+            <AnimatePresence>
+                {isOpen && (
+                    <div className="absolute z-50 top-36 w-full">
+                        <ContextMenu />
+                    </div>
+                )}
+            </AnimatePresence>
+
+            <div className="flex flex-row mx-8 gap-2 mt-8 items-center" onClick={() => setIsOpen(!isOpen)}>
+                <motion.div animate={{ rotate: isOpen ? 90 : 0 }} transition={{ duration: 0.2 }}>
+                    <HugeiconsIcon icon={ArrowRight01Icon} />
+                </motion.div>
 
                 <div>
                     <p className="font-BeVietnamBold text-xl">THÔNG TIN CÁ NHÂN</p>
@@ -30,9 +80,11 @@ function SettingsProfile() {
             </div>
 
             <div className="mt-4 flex flex-row justify-center gap-4">
-                <div className="px-6 py-2 font-BeVietnamRegular border-1 border-gray-400 rounded-lg">Hủy</div>
+                <div className="px-6 py-2 font-BeVietnamRegular border-1 border-gray-400 rounded-lg" >Hủy</div>
                 <Button label="Lưu" onClick={()=>{}} />
             </div>
+
+            <Footer />
         </div>
     )
 }
